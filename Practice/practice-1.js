cdb9a1d5094645d8b6aa82416edb392d5ddb30a7
noman-sajid <noman15072002@gmail.com>
2025-09-09 12:58:41 +0500

    JavaScript Baiscs

---
+++ Practice/practice-1.js
@@ -0,0 +1,13 @@
+// Problem 1: Create a variable of type string and add a number to it 
+let a = "aabbcc";
+let b = 6;
+let c = "3";
+console.log(a + b); //string
+console.log(c + b); //string
+console.log(b + c); //string
+console.log(c - b) //number
+
+typeof (a + b); //string
+typeof (c + b); //string
+typeof (b + c); //string
+typeof(c - b) //number
