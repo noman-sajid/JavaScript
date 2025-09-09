1d8e4bbe52f86d377761852ab386d26f1b299a02
noman-sajid <noman15072002@gmail.com>
2025-09-09 13:02:56 +0500

    Basics-1-1

--- Practice/practice-1.js
+++ Practice/practice-1.js
@@ -11,3 +11,19 @@ typeof (a + b); //string
 typeof (c + b); //string
 typeof (b + c); //string
 typeof(c - b) //number
+
+// Problem 2 create a const object in javascript can you change it to hold a number later
+  const obj = {
+    name : "joe",
+    age : 20
+  }
+ 
+  console.log(obj)
+
+  //obj = 3 // we cannot do this as obj is a constant
+
+  //but we can change the properties of the object
+ 
+  obj.name = "jerry";
+
+  console.log(obj) //Yes we certainly can change the values within the object 
