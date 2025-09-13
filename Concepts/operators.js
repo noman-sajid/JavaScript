2899e9bf87a7fffc71f310fb73e82ab3345b4206
noman-sajid <noman15072002@gmail.com>
2025-09-13 19:19:50 +0500

    Operators-AssignmentOperators

--- Concepts/operators.js
+++ Concepts/operators.js
@@ -11,3 +11,21 @@ console.log(a++); //post-increment
 console.log(a--); //post-decrement
 console.log(++a); //pre-increment   
 console.log(--a); //pre-decrement
+
+// Assignment Operators
+console.log(a == b); //equality
+console.log(a != b); //inequality
+console.log(a === b); //strict equality
+console.log(a !== b); //strict inequality
+console.log(a = b); //assignment
+console.log(a += b); //addition assignment
+console.log(a -= b); //subtraction assignment
+console.log(a *= b); //multiplication assignment
+console.log(a /= b); //division assignment
+console.log(a %= b); //modulus assignment
+console.log(a **= b); //exponentiation assignment
+console.log(a <<= b); //left shift assignment
+console.log(a >>= b); //right shift assignment
+console.log(a >>>= b); //zero-fill right shift assignment
+
+
