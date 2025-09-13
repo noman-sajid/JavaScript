4b9cd33424ca4ec279677e57ec9e0cddff65410d
noman-sajid <noman15072002@gmail.com>
2025-09-13 19:05:01 +0500

    Operators-1

---
+++ Concepts/operators.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b = 4;
+
+console.log(a + b) //addition 
+console.log(a - b) //subtraction
+console.log(a * b) //multiplication
+console.log(a / b) //division
+console.log(a % b) //modulus
+console.log(a ** b) //exponentiation
+console.log(a++); //post-increment
+console.log(a--); //post-decrement
+console.log(++a); //pre-increment   
+console.log(--a); //pre-decrement
