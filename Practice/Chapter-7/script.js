096b63689c51005d3472c41b36ae0cee341a2d0f
noman-sajid <noman15072002@gmail.com>
2025-11-19 22:19:04 +0500

    Practice-Chapter-7-Completed

--- Practice/Chapter-7/script.js
+++ Practice/Chapter-7/script.js
@@ -1,3 +1,12 @@
 const navbar = document.getElementsByClassName("navbar")[0];
+const listItems = document.getElementsByClassName("nav-items")[0];
+
+listItems.style.backgroundColor = "cyan";
 
 navbar.style.backgroundColor = "red";
+
+const container = document.querySelector(".container");
+const kids = container.children; 
+
+kids[0].style.backgroundColor = "green"; 
+kids[kids.length - 1].style.backgroundColor = "green"; 
