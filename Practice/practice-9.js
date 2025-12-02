1dc3d69d7884099f492babeb06f7fa3ef65f31d7
noman-sajid <noman15072002@gmail.com>
2025-12-02 20:35:30 +0500

    Practice-9

---
+++ Practice/practice-9.js
@@ -0,0 +1,39 @@
+
+
+// 👉 Task:
+// Write a function that returns an array of names sorted by age (ascending).
+
+// Expected output:
+
+// ["Sara", "Noman", "Ali"]
+
+
+const users = {
+    a: { name: "Noman", age: 23 },
+    b: { name: "Ali", age: 30 },
+    c: { name: "Sara", age: 19 }
+};
+
+function getNamesSortedByAgeLoop(users) {
+  const usersArray = Object.values(users);
+
+  for (let i = 0; i < usersArray.length - 1; i++) {
+    for (let j = 0; j < usersArray.length - 1 - i; j++) {
+      if (usersArray[j].age > usersArray[j + 1].age) {
+        let temp = usersArray[j];
+        usersArray[j] = usersArray[j + 1];
+        usersArray[j + 1] = temp;
+      }
+    }
+  }
+
+  const result = [];
+  for (let i = 0; i < usersArray.length; i++) {
+    result.push(usersArray[i].name);
+  }
+
+  return result;
+}
+
+
+
