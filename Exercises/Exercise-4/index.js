90ca23a58072901e18c44543f957fc8a83f8ecd0
noman-sajid <noman15072002@gmail.com>
2025-12-03 20:01:04 +0500

    Exercise-4-JavaScript_Clock-3

--- Exercises/Exercise-4/index.js
+++ Exercises/Exercise-4/index.js
@@ -1,19 +1,22 @@
 const clockElements = {
   hour: document.querySelector(".hour"),
   minute: document.querySelector(".minute"),
-  second: document.querySelector(".second")
+  second: document.querySelector(".second"),
+  ampmIndicator: document.querySelector(".ampm-indicator")
 };
 
 function updateClock() {
    const now = new Date();
-   const second = now.getSeconds();
+   const seconds = now.getSeconds();
    const rawHours = now.getHours();
    const minutes = now.getMinutes();
    const hours = rawHours % 12 || 12;
+   const ampm = rawHours >= 12 ? 'PM' : 'AM';
    clockElements.hour.textContent = String(hours).padStart(2, '0');
    clockElements.minute.textContent = String(minutes).padStart(2, '0');
-   clockElements.second.textContent = String(second).padStart(2, '0');
-   setTimeout(updateClock, 1000);
+   clockElements.second.textContent = String(seconds).padStart(2, '0');
+   clockElements.ampmIndicator.textContent = ampm;
+   setInterval(updateClock, 1000);
 }
 
 updateClock();
