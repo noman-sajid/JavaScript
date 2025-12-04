e6a83d62f2eaa5e7fe8d67f7d8bb3d221f632bee
noman-sajid <noman15072002@gmail.com>
2025-12-04 22:27:54 +0500

    Basics-JS-Callbacks-p-1

--- Concepts/callbacks.js
+++ Concepts/callbacks.js
@@ -15,3 +15,16 @@ fetchData(displayData); // Passing displayData as a callback
 
 // Another example with array methods
 const numbers = [1, 2, 3, 4, 5];
+
+// Problems 
+// Problem 1: Create a funciton called greetUser that takes a username and a callback function. The function should call the callback with a greeting message that includes the username.
+
+function greetUser(username, callback){
+   const message = `Hello, ${username}! Welcome back.`;
+   callback(message);
+}
+
+function showGreeting(message){
+   console.log(message);
+}
+greetUser("Alice", showGreeting); // Output: Hello, Alice! Welcome back.
