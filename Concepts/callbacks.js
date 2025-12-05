38fa1aa90e849617910eca96903daadfb3e8cb36
noman-sajid <noman15072002@gmail.com>
2025-12-05 09:35:30 +0500

    Basics-JS-Callbacks-p-4

--- Concepts/callbacks.js
+++ Concepts/callbacks.js
@@ -64,3 +64,33 @@ function multiplyFunc(result){
 
 calculate(2, 4 ,logFunc)
 calculate(2, 4 , multiplyFunc)
+
+// Problem 3: given an array of numbers, call forEach on it and log every number, log the index of each number, and log the square of each number using callback functions.
+
+
+const numbers1 = [2, 4, 6, 8];
+
+numbers.forEach((number, index)=>{
+  console.log("Number: " + number);
+  console.log("Index: " + index)
+  console.log("Square: " + number*number)
+})
+
+
+// Problem 4: create a function that takes an array of numbers and returns a new array with each number doubled and a new array with each number converted to a string using the map method and callback functions.
+const nums = [1, 2, 3, 4, 5];
+
+const doubled = nums.map((num) => num * 2);
+console.log("Doubled: ", doubled);
+const stringified = nums.map((num) => num.toString());
+console.log("Stringified: ", stringified);
+
+// Problem 5: create a function that takes and array of numbers and get numbers abouve a certain threshold using the filter method and a callback function.
+
+const values = [10, 25, 30, 45, 50, 60];
+const threshold = 18;
+
+const filteredValues = values.filter((value) => value > threshold);
+console.log("Filtered Values: ", filteredValues);
+
+
