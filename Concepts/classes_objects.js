ac85ae4f4d124ce4bf31578e712a7e605d1331fe
noman-sajid <noman15072002@gmail.com>
2025-12-15 20:53:50 +0500

    JavaScript_Objects_and_Classes-5

--- Concepts/classes_objects.js
+++ Concepts/classes_objects.js
@@ -54,28 +54,92 @@ const car = {
 
 //this keyword in context of classes and objects
 // The this keyword in JavaScript refers to the object that is currently executing the code. In the context of classes and objects, this typically refers to the instance of the class that is calling a method or accessing a property.
-class RailwayTicket {
-    submit(){
-        alert(this.name +  ": Your Form is submited");
-    }
-    cancel(){
-        alert(this.name +  ": Your Form is cancelled");
-    }
-    fill(name){
-      this.name = name;   
-    }
-}
+// class RailwayTicket {
+//      constructor(){
+//         console.log("Constructor Called!")
+//      }
+//     submit(){
+//         alert(this.name +  ": Your Form is submited");
+//     }
+//     cancel(){
+//         alert(this.name +  ": Your Form is cancelled");
+//     }
+//     fill(name, train_no){
+//       this.name = name;  
+//       this.train_no = train_no;  
+//     }
+// }
 
-let alice = new RailwayTicket();
-let bob = new RailwayTicket();
-alice.fill("Alice");
-bob.fill("Bob");
-alice.submit();
-bob.submit();
-alice.cancel();
+// let alice = new RailwayTicket();
+// let bob = new RailwayTicket();
+// alice.fill("Alice", 121212);
+// bob.fill("Bob", 1215415);
+// alice.submit();
+// bob.submit();
+// alice.cancel();
 
 
 //under the hood javaScript is doing:
   this = alice
   this.name = "Alice"
   alert(this.name +  ": Your Form is submited");
+
+
+  // Intro to constructors 
+  // defination: A constructor is a special method in a class that is automatically called when a new object is created, and is used to initialize the object.
+  //Important points about constructors:
+  //1. The constructor method is defined using the keyword constructor.
+  //2. It can take parameters to initialize the object's properties.
+  //3. If no constructor is defined, a default constructor is provided by JavaScript.
+  //4. The constructor method is called automatically when a new instance of the class is created using the new keyword.
+
+  //  wrong
+alice.constructor("Alice");
+
+// Example --correct
+  class RailwayTicket {
+     constructor(name, train_no, address){
+        console.log("Constructor Called!")
+         this.name = name;  
+         this.train_no = train_no;  
+         this.address = address; 
+     }
+    submit(){
+        alert(this.name +  ": Your Form is submited");
+    }
+    cancel(){
+        alert(this.name +  ": Your Form is cancelled");
+    }
+   
+}
+
+let alice = new RailwayTicket('Alice', 121212, "221 Bakers Street London");
+let bob = new RailwayTicket('Bob', 1215415, '232 Veteran Street London');
+
+alice.submit();
+bob.submit();
+alice.cancel();
+
+//constructor is used to describe what an object should look like and also to initialize the object with some values when it is created.
+// methods are used to define the behavior of the object, i.e., what actions the object can perform.
+
+// adding new cunstructor
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    job(title) {
+        this.title = title;
+        console.log(`${this.name} is a ${this.title}.`);
+    }
+}
+
+const person12 = new Person("Alice", 30);
+person1.job("Engineer"); // Output: Alice is a Engineer.
+
+// In this example, the Person class has a constructor that initializes the name and age properties, and a job method that sets the title property and logs a message. When we create a new instance of Person, we can call the job method to define the person's job title.
+
+// In summary, constructors are special methods used to initialize objects when they are created, while methods define the actions that objects can perform.
+
+
