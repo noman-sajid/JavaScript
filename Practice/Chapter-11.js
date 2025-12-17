d787f44e53a26a0a10753641624dbdc0d9873f0b
noman-sajid <noman15072002@gmail.com>
2025-12-17 20:25:53 +0500

    JavaScript_Practice_Set_Chapter-11

---
+++ Practice/Chapter-11.js
@@ -0,0 +1,63 @@
+// Chapter 11 Practice Set --Classes and Objects
+
+// Problem 1: Create a JavaScript class to create a complex numbers create a constructor to set the real and imagenary part
+
+// class Complex {
+//     constructor(real, imagenary){
+//         this.real = real;
+//         this.imagenary = imagenary;
+//     }
+
+// }
+
+// let a = new Complex(2, 4);
+// let b = new Complex(6, 8);
+
+// Problem 2 : Create a method add to add two complex numbers
+
+  
+
+class Complex {
+    constructor(real, imagenary){
+        this.real = real;
+        this.imagenary = imagenary;
+    }
+    add(otherComplex){
+        let realPart = this.real + otherComplex.real;
+        let imagenaryPart = this.imagenary + otherComplex.imagenary;
+        return new Complex(realPart, imagenaryPart)
+    }
+}
+
+let a = new Complex(2, 4); // Output: 2 + 4i
+let b = new Complex(6, 8); // Output: 6 + 8i
+let c = a.add(b);
+console.log(`Result: ${c.real} + ${c.imagenary}i`); // Result: 8 + 12i
+
+
+// Problem 3: Create a class student from a class human and override a method to see changes.
+
+class Human {
+    constructor(name, job){
+        this.name = name;
+        this.job = job;
+    }
+    walk(){
+        console.log(`human is walking`)
+    }
+}
+
+class Student extends Human {
+    constructor(name, job){
+        super(name, job)
+    }
+    walk(){
+       console.log(`Student is walking`)
+    }
+
+}
+
+let s1 = new Student("Josh", 23)
+s1.walk()
+
+
