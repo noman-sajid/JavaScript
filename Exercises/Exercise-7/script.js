aece68f585442b7c507b10e37cb9476f7bc07344
noman-sajid <noman15072002@gmail.com>
2025-12-23 21:17:15 +0500

    Exercise-7-Password_Generator-3

--- Exercises/Exercise-7/script.js
+++ Exercises/Exercise-7/script.js
@@ -4,27 +4,41 @@ const generateBtn = document.getElementsByClassName("generate-btn")[0];
 const passwordValues = document.querySelectorAll(".password-value");
 
 class Password {
-  constructor(length) {
-    this.length = length;
-    this.chars =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-  }
-    generate() {
-    let password = "";
-    for (let i = 0; i < this.length; i++) {
-        const randomIndex = Math.floor(Math.random() * this.chars.length);
-        password += this.chars[randomIndex];
+    constructor(length = 10, addUppercase, addNumer, addLowerCase, addSpecialChar){
+        this.length = length; 
+        this.addLowerCase = addLowerCase;
+        this.addNumer = addNumer;
+        this.addSpecialChar = addSpecialChar;
+        this.addUppercase = addUppercase;
     }
-    return password;
-  }
+    generate(){
+        let charSet = "";
+        const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+        const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const numberChars = "0123456789";
+        const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+        if(this.addLowerCase){
+            charSet += lowerCaseChars;
+        }
+        if(this.addUppercase){
+            charSet += upperCaseChars;
+        }
+        if(this.addNumer){
+            charSet += numberChars;
+        }
+        if(this.addSpecialChar){
+            charSet += specialChars;
+        }
+        if(charSet === ""){
+            return new Error("At least one character type must be selected");
+        }
+        let password = "";
+        
+        for(let i = 0; i < this.length; i++){
+            const randomIndex = Math.floor(Math.random() * charSet.length);
+            password += charSet[randomIndex];
+        }
+        return password;
+    }
 }
 
-generateBtn.addEventListener("click", () => {
-  const weakPassword = new Password(6);
-  const mediumPassword = new Password(10);
-  const strongPassword = new Password(16);
-  passwordValues[0].textContent = weakPassword.generate();
-    passwordValues[1].textContent = mediumPassword.generate();
-    passwordValues[2].textContent = strongPassword.generate();
-});
-    
