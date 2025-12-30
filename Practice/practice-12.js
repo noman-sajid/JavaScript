9c993f071cb6e4d06deb1e7588e72e43a7d276fe
noman-sajid <noman15072002@gmail.com>
2025-12-30 06:32:13 +0500

    JavaScript-Practice-2

--- Practice/practice-12.js
+++ Practice/practice-12.js
@@ -237,3 +237,30 @@ function convertTemp(n){
 }
 
 console.log(convertTemp(32));
+
+
+
+
+// //Calculate the average of the 
+// numbers in an array of numbers
+//  Example array:
+//  [1, 3, 9, 15, 90]
+//  Expected output:
+//  23.6
+
+
+let givenArray =  [1, 3, 9, 15, 90];
+let sum = 0;
+function calcAvg(arr){
+    let arrayLength = arr.length; 
+ 
+    for(el of arr){
+     sum += el
+    }
+ let avg = sum / arrayLength ;
+    return avg;
+    
+}
+
+console.log(calcAvg(givenArray))
+
