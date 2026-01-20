f86812c7da8794c10819c93499c7ca16d639861a
noman-sajid <noman15072002@gmail.com>
2026-01-20 22:51:01 +0500

    Two Sum Sorted Array Solutions

--- Quick Surge/surge-2.js
+++ Quick Surge/surge-2.js
@@ -93,3 +93,27 @@ function firstUnique(str){
 
 firstUnique("aabbccdeff")
 
+
+
+// Two Sum Problem --Sorted Array
+  
+
+function twoSum(arr, target){
+    let left = 0;
+    let right = arr.length - 1; 
+  while(left < right){
+    console.log(left, right)
+    const sum = arr[left] + arr[right]
+    if(sum === target){
+      return [left  , right ]
+    } else if(sum < target){
+      left++
+    } else {
+      right--
+    }
+  }
+  return []
+}
+
+twoSum([2, 7, 11, 15], 9) // [0, 1]
+console.log(twoSum([2, 7, 11, 15], 9)) // [0, 1]
