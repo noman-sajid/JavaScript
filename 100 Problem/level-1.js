8bcf2d031ff6a8e20cdf437d2dd8e03b4ad3dee1
noman-sajid <noman15072002@gmail.com>
2026-02-17 22:45:28 +0500

    JavaScript-100-Problems_p-3-4

--- 100 Problem/level-1.js
+++ 100 Problem/level-1.js
@@ -20,3 +20,33 @@ function checkEvenOdd(num){
 
 console.log(checkEvenOdd(2))
 console.log(checkEvenOdd(5))
+
+
+
+// Problem: 3 Find max of two numbers
+
+function findMax(a, b){
+    if(a > b ){
+   return a
+    } else {
+     return b 
+    }
+}
+
+// const findMax = (a, b) => Math.max(a, b )
+
+console.log(findMax(2 , 4 ))
+console.log(findMax(6 , 4 ))
+
+
+// Problem 4: Celsius to Fahrenheit
+
+function celToFar(deg){
+    return (deg * 9/5 ) + 32
+}
+
+//const celToFar = (deg) => (deg * 9/5) + 32;
+
+console.log(celToFar(0)) // 32
+console.log(celToFar(2)) // 35.6
+
